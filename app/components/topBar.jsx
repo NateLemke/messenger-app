8fbf1b568233dda146188f4d27f66a76a5ba8a64
Nate Lemke <Nateglemke@gmail.com>
2024-10-18 02:37:16 -0700

    Centered profile on smaller screens

--- app/components/topBar.jsx
+++ app/components/topBar.jsx
@@ -11,7 +11,7 @@ export default function TopBar({toggleMenu}) {
                 <IoMenu className=" text-3xl cursor-pointer hover:scale-110 
                 hover:opacity-50" onClick={toggleMenu}/>
             </div>
-            {chatPartner && <div className="flex flex-row justify-center items-center max-w-80 max-sm:mx-auto ml-2">
+            {chatPartner && <div className="flex flex-row justify-center items-center max-w-80 max-sm:mx-auto max-sm:absolute ml-2">
                 <img className="sm:w-14 sm:h-14 w-10 h-10 rounded-full mr-2" src={chatPartner.avatar?chatPartner.avatar:"/defaultUser.png"}/>
                 <div className="flex flex-col">
                     <h2 className="max-sm:text-xs">{chatPartner.username}</h2>
